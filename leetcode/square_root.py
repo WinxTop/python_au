ffbba76530f18aaa52330362964f84d6075efc25
Kirill <kir.karasyov2017@yandex.ru>
2020-10-18 19:34:04 +0300

    Added a solution for square root task

---
+++ leetcode/square_root.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        a = 0
+        b = x
+        i=1
+        while i < 1000:
+            mid = (a+b)/2
+            if mid**2 < x:
+                a = mid
+            if mid**2 > x:
+                b = mid
+            i = i + 1
+        check = int(math.floor(mid))
+        if (check + 1)**2 == x:
+            return int(check + 1)
+        else:
+            return int(math.floor(mid))
